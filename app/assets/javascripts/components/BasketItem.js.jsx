d80bc572c9357fefbd092329fe8f6330522c80bc
danmitchell- <dan@digitalreflow.co.uk>
2016-04-21 08:29:09 +0100

    add key to basket items row

--- app/assets/javascripts/components/BasketItem.js.jsx
+++ app/assets/javascripts/components/BasketItem.js.jsx
@@ -5,7 +5,7 @@ var BasketItem = React.createClass({
         {
           this.props.items.map(function(item) {
             return (
-              <tr className="basket-item">
+              <tr className="basket-item" key={item.id}>
                 <td className="basket-item__name">{item.name}</td>
                 <td className="basket-item__code">{item.code}</td>
                 <td className="basket-item__quantity"><QuantityInput /></td>
