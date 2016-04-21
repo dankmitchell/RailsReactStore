3a2d426fa69aaf985d7537e5e1a2443dd9eb0c64
danmitchell- <dan@digitalreflow.co.uk>
2016-04-21 15:12:55 +0100

    update styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Product.delete_all
-Product.create! name: "Jeans", code: "J01", price: 32.95, image_url: "http://asset1.cxnmarksandspencer.com/is/image/mands/SD_01_T50_8306_T0_X_EC_90"
-Product.create! name: "Blouse", code: "B01", price: 24.95, image_url: "http://asset1.marksandspencer.com/is/image/mands/SD_01_T50_4994_P6_X_EC_0"
-Product.create! name: "Socks", code: "S01", price: 7.95, image_url: "http://asset1.cxnmarksandspencer.com/is/image/mands/SD_02_T60_9461_T4_X_EC_0"
+Product.create! name: "Jeans", code: "J01", price: 32.95, image_url: "//asset1.cxnmarksandspencer.com/is/image/mands/SD_01_T50_8306_T0_X_EC_90"
+Product.create! name: "Blouse", code: "B01", price: 24.95, image_url: "//asset1.marksandspencer.com/is/image/mands/SD_01_T50_4994_P6_X_EC_0"
+Product.create! name: "Socks", code: "S01", price: 7.95, image_url: "//asset1.cxnmarksandspencer.com/is/image/mands/SD_02_T60_9461_T4_X_EC_0"
